04479815821fedf942fbe746f5dc46aac23e9a4a
tcje91 <tcje9117@outlook.com>
2019-04-15 00:55:59 +0100

    add style active link functionality

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,19 +5,24 @@ import NavItems from './NavItems';
 export default class Menu extends Component {
   state = {
     showMenu: false,
+    selected: 'home',
   }
 
   toggleMenu = () => {
     this.setState(prevState => ({ showMenu: !prevState.showMenu }));
   }
 
+  updateSelected = (page) => {
+    this.setState({ selected: page });
+  }
+
   render() {
-    const { showMenu } = this.state;
+    const { showMenu, selected } = this.state;
 
     return (
-      <div className="menu">
+      <div className={`menu ${showMenu ? 'open' : ''}`}>
         <MenuBtn showMenu={showMenu} toggleMenu={this.toggleMenu} />
-        <NavItems showMenu={showMenu} />
+        <NavItems showMenu={showMenu} selected={selected} updateSelected={this.updateSelected} />
       </div>
     );
   }
--- src/components/NavItems.js
+++ src/components/NavItems.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Link } from '@reach/router';
 
-const NavItems = ({ showMenu }) => (
+const NavItems = ({ showMenu, selected, updateSelected }) => (
   <div className="nav-list">
-    <Link to="/" className={`nav-item ${showMenu ? 'open' : ''}`}>Home</Link>
+    <Link to="/" onClick={() => updateSelected('home')} className={`nav-item ${showMenu ? 'open' : ''} ${selected === 'home' ? 'selected' : ''}`}>Home</Link>
     <br />
-    <Link to="/work" className={`nav-item ${showMenu ? 'open' : ''}`}>Work</Link>
+    <Link to="/work" onClick={() => updateSelected('work')} className={`nav-item ${showMenu ? 'open' : ''} ${selected === 'work' ? 'selected' : ''}`}>Work</Link>
     <br />
-    <Link to="/about" className={`nav-item ${showMenu ? 'open' : ''}`}>About</Link>
+    <Link to="/about" onClick={() => updateSelected('about')} className={`nav-item ${showMenu ? 'open' : ''} ${selected === 'about' ? 'selected' : ''}`}>About</Link>
     <br />
-    <Link to="/contact" className={`nav-item ${showMenu ? 'open' : ''}`}>Contact</Link>
+    <Link to="/contact" onClick={() => updateSelected('contact')} className={`nav-item ${showMenu ? 'open' : ''} ${selected === 'contact' ? 'selected' : ''}`}>Contact</Link>
   </div>
 );
 
