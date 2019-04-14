ac6f012228cfcc03c011cd8e67af30e6fe663a46
tcje91 <tcje9117@outlook.com>
2019-04-14 15:42:03 +0100

    add icons to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as BrandIcons from '@fortawesome/free-brands-svg-icons';
+
 
 const Home = () => (
-  <div>
+  <div className="home">
     <h1>Home</h1>
+    <h1 className="lrg-heading">
+      Joe
+      {' '}
+      <span className="text-highlight">Bloggs</span>
+    </h1>
+    <FontAwesomeIcon icon={BrandIcons.faGithub} size="2x" className="icon" />
+    <FontAwesomeIcon icon={BrandIcons.faLinkedin} size="2x" className="icon" />
   </div>
 );
 
