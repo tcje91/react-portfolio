a252dd6afd8f09a4fe83401f9e27f35577462021
tcje91 <tcje9117@outlook.com>
2019-04-15 18:01:24 +0100

    rotate close btn

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import * as BrandIcons from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => (
   <div className="footer">
-    <span className="footer-text">Created by Joe Bloggs</span>
+    <span className="footer-text">Built by Joe Bloggs</span>
     <FontAwesomeIcon icon={BrandIcons.faReact} size="2x" className="icon" />
     <FontAwesomeIcon icon={BrandIcons.faSass} size="2x" className="icon" />
     <FontAwesomeIcon icon={BrandIcons.faHtml5} size="2x" className="icon" />
