827602293ea560aacd159ce8234b622613b6ff8a
tcje91 <tcje9117@outlook.com>
2019-04-16 14:47:51 +0100

    add images to project details

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const ProjectDetails = ({ onCloseProject, show, project }) => (
   <div className={`project-details ${show ? 'show' : ''}`}>
+    {project && <div className="img-wrapper-med"><img src={`/images/${project.imgMed ? project.imgMed : 'lemonade-med.png'}`} alt="img" className="project-img-med" /></div>}
     <h1 className="project-title">{project ? project.title : 'no project'}</h1>
     <p className="project-desc">{project ? project.desc : 'no project'}</p>
     <div className="close-btn" onClick={onCloseProject}>
