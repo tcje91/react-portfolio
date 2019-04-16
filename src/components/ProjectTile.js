c2b841364debf555182c1179864dad61149de152
tcje91 <tcje9117@outlook.com>
2019-04-16 14:26:15 +0100

    create overlay for project images

--- src/components/ProjectTile.js
+++ src/components/ProjectTile.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const ProjectTile = ({ project, onClickProject }) => (
   <div className="project-tile" onClick={() => onClickProject(project)}>
-    <div className="project-img" />
+    {/* <div className="project-img-div" /> */}
+    <div className="img-container">
+      <div className="img-overlay" />
+      <img src={`/images/${project.imgSml ? project.imgSml : 'lemonade-sml.png'}`} alt="img" className="project-img" />
+    </div>
     <p className="project-tile-title">{project.title}</p>
   </div>
 );
--- src/data.js
+++ src/data.js
@@ -1,13 +1,17 @@
 export const projects = [
   {
     id: 1,
-    title: 'project1',
+    title: 'Lemonade Tycoon',
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolor eum alias asperiores, earum labore pariatur officia sunt expedita cumque sed quis quasi eligendi. Illo consectetur neque, ad, reiciendis iste voluptate quae, itaque debitis nobis ex ratione consequatur. Nobis velit consequatur culpa maiores dolore error sint nostrum quidem hic alias.',
+    imgSml: 'lemonade-sml.png',
+    imgMed: 'lemonade-med.png',
   },
   {
     id: 2,
-    title: 'project2',
+    title: 'NC News',
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolor eum alias asperiores, earum labore pariatur officia sunt expedita cumque sed quis quasi eligendi. Illo consectetur neque, ad, reiciendis iste voluptate quae, itaque debitis nobis ex ratione consequatur. Nobis velit consequatur culpa maiores dolore error sint nostrum quidem hic alias.',
+    imgSml: 'ncnews-sml.png',
+    imgMed: 'ncnews-med.png',
   },
   {
     id: 3,
