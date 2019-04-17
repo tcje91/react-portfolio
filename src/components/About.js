f8d9d50f022b30093bd3d511fe1aebc4baff0330
tcje91 <tcje9117@outlook.com>
2019-04-17 15:23:23 +0100

    make menu btn more visible on mobile

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const About = () => (
   <div className="about content-container">
-    <h1 className="lrg-heading">About Me</h1>
+    <h1 className="lrg-heading">
+      About
+      {' '}
+      <span className="text-highlight">Me</span>
+    </h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto dolor eum alias asperiores, earum labore pariatur officia sunt expedita cumque sed quis quasi eligendi. Illo consectetur neque, ad, reiciendis iste voluptate quae, itaque debitis nobis ex ratione consequatur. Nobis velit consequatur culpa maiores dolore error sint nostrum quidem hic alias.</p>
     <p>Suscipit ea maiores quibusdam reiciendis molestias, beatae consequuntur voluptatibus ipsum ipsam magni expedita vel veniam odit odio. Itaque recusandae enim accusantium possimus! Laudantium ad asperiores architecto, quasi sequi debitis qui in porro. Est illo provident ab quaerat perferendis maxime culpa totam enim, dolore quibusdam, maiores eius expedita ea, magnam animi.</p>
     <p>Repellendus aperiam harum quos aliquam dolores adipisci eum quo eaque ullam quas qui iure deserunt id suscipit totam quis voluptatibus corrupti obcaecati iusto voluptatum inventore, nam ad a in? Sed, hic. Facilis corporis amet expedita itaque totam rem, quaerat repudiandae, quisquam dignissimos exercitationem et illum distinctio sit ratione cumque dicta!</p>
