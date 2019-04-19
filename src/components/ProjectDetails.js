b8cb0248218e7cc5f4b2a262eb3ebf29488472cb
tcje91 <tcje9117@outlook.com>
2019-04-19 14:50:41 +0100

    add text tech stack to projects

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -11,12 +11,13 @@ const ProjectDetails = ({ onCloseProject, show, project }) => (
         <img src={`/images/${project.imgMed ? project.imgMed : 'lemonade-med.png'}`} alt="img" className="project-img-med" />
       </div>
       <h1 className="project-title">{project.title}</h1>
+      <p className="tech-list">{project.techListStr}</p>
       <p className="project-desc">{project.desc ? project.desc : project.descTest}</p>
       <div className="url-icons-wrapper">
         {project.githubUrl && <a href={project.githubUrl} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={BrandIcons.faGithub} className="icon" size="2x" /></a>}
         {project.hostedUrl && <a href={project.hostedUrl} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={SolidIcons.faExternalLinkAlt} className="icon" size="2x" /></a>}
       </div>
-      <div className="tech-list">
+      <div className="tech-list-icons">
         {project.techList.map(tech => <FontAwesomeIcon icon={BrandIcons[tech]} key={tech} className="icon" size="2x" />)}
       </div>
       <div className="close-btn" onClick={onCloseProject}>
