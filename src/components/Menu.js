e864a5546a7391353e2c5336e60c00dc95820b7f
tcje91 <tcje9117@outlook.com>
2019-04-20 15:05:02 +0100

    make active link highlight functionality more robust

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,29 +1,29 @@
 import React, { Component } from 'react';
+import { Location } from '@reach/router';
 import MenuBtn from './MenuBtn';
 import NavItems from './NavItems';
 
 export default class Menu extends Component {
   state = {
     showMenu: false,
-    selected: 'home',
   }
 
   toggleMenu = () => {
     this.setState(prevState => ({ showMenu: !prevState.showMenu }));
   }
 
-  updateSelected = (page) => {
-    this.setState({ selected: page, showMenu: false });
-  }
-
   render() {
-    const { showMenu, selected } = this.state;
+    const { showMenu } = this.state;
 
     return (
-      <div className={`menu ${showMenu ? 'open' : ''}`}>
-        <MenuBtn showMenu={showMenu} toggleMenu={this.toggleMenu} />
-        <NavItems showMenu={showMenu} selected={selected} updateSelected={this.updateSelected} />
-      </div>
+      <Location>
+        {({ location: { pathname } }) => (
+          <div className={`menu ${showMenu ? 'open' : ''}`}>
+            <MenuBtn showMenu={showMenu} toggleMenu={this.toggleMenu} />
+            <NavItems showMenu={showMenu} toggleMenu={this.toggleMenu} path={pathname} />
+          </div>
+        )}
+      </Location>
     );
   }
 }
